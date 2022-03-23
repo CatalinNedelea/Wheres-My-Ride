15e791b753c82e5db3f12fb0fc3a2c526f401554
CatalinNedelea <nedelea.catalin98@gmail.com>
2022-03-23 20:42:20 +0200

    Fixed Serial Extractor

--- gps_modules/serial_extractor.py
+++ gps_modules/serial_extractor.py
@@ -1,13 +1,19 @@
 def getserial():
-    # Extract serial from cpuinfo file
-    cpuserial = "0000000000000000"
-    try:
-        f = open('/proc/cpuinfo', 'r')
-        for line in f:
-            if line[0:6] == 'Serial':
-                cpuserial = line[10:26]
-        f.close()
-    except:
-        cpuserial = "ERROR000000000"
+	# Extract serial from cpuinfo file
+	cpuserial = "0000000000000000"
+	try:
+		f = open('/proc/cpuinfo', 'r')
+		for line in f:
+			if line[0:6] == 'Serial':
+				cpuserial = line[10:26]
+		f.close()
+	except:
+		cpuserial = "ERROR000000000"
 
-    return cpuserial
+	return cpuserial
+
+myserial = getserial()
+
+with open('serialNo.txt', 'w') as f:
+    for line in myserial:
+        f.write(line)
