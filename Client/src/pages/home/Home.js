8b904ba9bd1ca18a4be4ed8d374232ddb97f5eaa
CatalinNedelea <69271126+CatalinNedelea@users.noreply.github.com>
2022-06-21 00:17:18 +0300

    Updated map logic

--- Client/src/pages/home/Home.js
+++ Client/src/pages/home/Home.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "../../axios/axios";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
-  InfoWindow,
+  Autocomplete,
+  DirectionsRenderer,
 } from "@react-google-maps/api";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import { formatRelative } from "date-fns";
-import "@reach/combobox/styles.css";
 import mapStyles from "./mapStyles";
-import bear from "../../environment/assets/bear.svg";
 import compass from "../../environment/assets/compass.svg";
+import { parseLatitude, parseLongitude } from "../../helpers/DDMtoDD";
+import { Frame, Center, Utility, MapFrame } from "./Home.style";
 
 const libraries = ["places"];
-const mapContainerStyle = {
-  height: "100vh",
-  width: "100vw",
-};
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
@@ -45,9 +34,42 @@ export default function Home() {
   });
   const [markers, setMarkers] = React.useState([]);
   const [selected, setSelected] = React.useState(null);
-
+  const [userLocation, setUserLocation] = React.useState();
   const [vehicles, setVehicles] = useState([]);
   const [error, setError] = useState(false);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef();
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
 
   useEffect(() => {
     axios
@@ -60,16 +82,24 @@ export default function Home() {
       });
   }, []);
 
-  const onMapClick = React.useCallback((e) => {
-    setMarkers((current) => [
-      ...current,
-      {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng(),
-        time: new Date(),
-      },
-    ]);
-  }, []);
+  useEffect(() => {
+    if (vehicles.length) {
+      vehicles.map((vehicle) => {
+        const check = markers.some((el) => el.id === vehicle._id);
+        if (!check) {
+          setMarkers((current) => [
+            ...current,
+            {
+              lat: parseLatitude(vehicle.currentLocation.latitude),
+              lng: parseLongitude(vehicle.currentLocation.longitude),
+              time: new Date(),
+              id: vehicle._id,
+            },
+          ]);
+        }
+      });
+    }
+  }, [vehicles]);
 
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
@@ -84,62 +114,85 @@ export default function Home() {
   if (loadError) return "Error";
   if (!isLoaded) return "Loading...";
 
+  function success(pos) {
+    const crd = pos.coords;
+    setUserLocation({
+      lat: crd.latitude,
+      lng: crd.longitude,
+      time: new Date(),
+    });
+  }
+
+  function error2(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error2);
+
   return (
-    <div>
-      <Locate panTo={panTo} />
-      <Search panTo={panTo} />
+    <Frame>
+      <Utility>
+        <Locate panTo={panTo} />
+        <div>
+          <Autocomplete>
+            <input type="text" placeholder="Origin" ref={originRef} />
+          </Autocomplete>
+        </div>
+        <div>
+          <Autocomplete>
+            <input type="text" placeholder="Destination" ref={destinationRef} />
+          </Autocomplete>
+        </div>
 
-      <GoogleMap
-        id="map"
-        mapContainerStyle={mapContainerStyle}
-        zoom={8}
-        center={center}
-        options={options}
-        onClick={onMapClick}
-        onLoad={onMapLoad}
-      >
-        {vehicles.map((marker) => (
-          <Marker
-            key={`${marker.currentLocation.latitude}-${marker.currentLocation.longitude}`}
-            position={{
-              lat: marker.currentLocation.latitude,
-              lng: marker.currentLocation.longitude,
-            }}
-            // onClick={() => {
-            //   setSelected(marker);
-            // }}
-            // icon={{
-            //   url: bear,
-            //   origin: new window.google.maps.Point(0, 0),
-            //   anchor: new window.google.maps.Point(15, 15),
-            //   scaledSize: new window.google.maps.Size(30, 30),
-            // }}
-          />
-        ))}
+        <div>
+          <button type="submit" onClick={calculateRoute}>
+            Calculate Route
+          </button>
+        </div>
 
-        {selected ? (
-          <InfoWindow
-            position={{ lat: selected.lat, lng: selected.lng }}
-            onCloseClick={() => {
-              setSelected(null);
-            }}
-          >
-            <div>
-              <h2>
-                <span role="img" aria-label="bear">
-                  🐻
-                </span>{" "}
-                Alert
-              </h2>
-              <p>Spotted {formatRelative(selected.time, new Date())}</p>
-            </div>
-          </InfoWindow>
-        ) : null}
-      </GoogleMap>
-    </div>
+        <div>
+          <p>Distance: {distance} </p>
+          <p>Duration: {duration} </p>
+        </div>
+      </Utility>
+
+      <MapFrame>
+        <GoogleMap
+          id="map"
+          mapContainerStyle={{ height: "100vh", width: "100vw" }}
+          zoom={8}
+          center={center}
+          options={options}
+          onLoad={onMapLoad}
+        >
+          {userLocation && (
+            <Marker
+              key={`${userLocation.lat}-${userLocation.lng}`}
+              position={{
+                lat: userLocation.lat,
+                lng: userLocation.lng,
+              }}
+            />
+          )}
+
+          {markers.map((marker) => (
+            <Marker
+              key={marker.id}
+              position={{ lat: marker.lat, lng: marker.lng }}
+            />
+          ))}
+
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </MapFrame>
+    </Frame>
   );
 }
 
+//Returns your current location
 function Locate({ panTo }) {
   return (
     <button
@@ -161,57 +214,52 @@ function Locate({ panTo }) {
   );
 }
 
-function Search({ panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 43.6532, lng: () => -79.3832 },
-      radius: 100 * 1000,
-    },
-  });
+// function Search({ panTo }) {
+//   const {
+//     ready,
+//     value,
+//     suggestions: { status, data },
+//     setValue,
+//     clearSuggestions,
+//   } = usePlacesAutocomplete({
+//     requestOptions: {
+//       location: { lat: () => 43.6532, lng: () => -79.3832 },
+//       radius: 100 * 1000,
+//     },
+//   });
 
-  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+//   const handleInput = (e) => {
+//     setValue(e.target.value);
+//   };
 
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
+//   const handleSelect = async (address) => {
+//     setValue(address, false);
+//     clearSuggestions();
 
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
+//     try {
+//       const results = await getGeocode({ address });
+//       const { lat, lng } = await getLatLng(results[0]);
+//       panTo({ lat, lng });
+//     } catch (error) {
+//       console.log("😱 Error: ", error);
+//     }
+//   };
 
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
-
-  return (
-    <div className="search">
-      <Combobox onSelect={handleSelect}>
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Search your location"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
-}
+//   return <div>Tomato</div>;
+//   /* <Combobox onSelect={handleSelect}>
+//         <ComboboxInput
+//           value={value}
+//           onChange={handleInput}
+//           disabled={!ready}
+//           placeholder="Search your location"
+//         />
+//         <ComboboxPopover>
+//           <ComboboxList>
+//             {status === "OK" &&
+//               data.map(({ id, description }) => (
+//                 <ComboboxOption key={id} value={description} />
+//               ))}
+//           </ComboboxList>
+//         </ComboboxPopover>
+//       </Combobox> */
+// }
---
+++ Client/src/pages/home/Home.style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Frame = styled.div`
+  position: relative;
+  justify-content: space-between;
+  flex-direction: row;
+  display: flex;
+`;
+export const Center = styled.div`
+  position: absolute;
+  top: 10%;
+  width: 100%;
+  text-align: center;
+  font-size: 18px;
+`;
+
+export const Utility = styled.div`
+  border-radius: 5rem;
+  background-color: red;
+  min-width: 5rem;
+  min-height: 5rem;
+  z-index: 1;
+`;
+
+export const MapFrame = styled.div``;
